9b0fce8a3df43c972454699057dd30d3b106bf56
1135428665jyk <42425263+1135428665jyk@users.noreply.github.com>
2020-05-07 06:24:23 +0800

    Create Demo.java

---
+++ Interface/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        TestDemo testDemo = new TestDemo();
+        testDemo.openDoor();
+        testDemo.open();
+        testDemo.closeDoor();
+        testDemo.close();
+
+    }
+}
